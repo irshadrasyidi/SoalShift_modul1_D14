8675cb920a877b579f142fdf3e4fc1ab51b378b8
irshadrasyidi <36858141+irshadrasyidi@users.noreply.github.com>
2019-03-02 12:33:12 +0700

    nghapusin komen

--- soal5.sh
+++ soal5.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 
-#awk '{print}' /var/log/syslog
-#awk '(/CRON/ || /cron/),!/sudo/' /var/log/syslog
-
 awk '(/CRON/ || /cron/),!/sudo/' /var/log/syslog | awk 'NF < 13' >> /home/irshadrasyidi/modul1/no5.txt
